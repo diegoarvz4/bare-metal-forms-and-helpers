7493d65e5a9d3d3a112f121066ed495770633bb1
ngodi <albertngodi@gmail.com>
2019-07-15 20:12:20 +0100

    edit form added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     
     def new
+        @user = User.new
     end
 
     def create
@@ -13,6 +14,13 @@ class UsersController < ApplicationController
           render :new
         end
     end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+    def update
+    end
+  
 
     private 
 
